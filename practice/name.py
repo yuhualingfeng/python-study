dc42c258a5659928b7759c8787a98294e9e95ea8
puhuasheng <1135317965@qq.com>
2018-10-29 23:07:47 +0800

    add practice

---
+++ practice/name.py
@@ -0,0 +1,38 @@
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print("\n");
+
+
+first_name = "ada"
+last_name = "lovelace"
+full_name = "ada lovelace"
+print(full_name)
+print('\n');
+
+favorite_language = ' python '
+print(favorite_language.rstrip())
+print(favorite_language.lstrip())
+print(favorite_language.strip())
+print('\n');
+
+username = 'Eric';
+print('Hello '+username+ 'would you like to learn some Python today?')
+print(username.lower())
+print(username.upper())
+print(username.title())
+print('\n')
+
+famous_person = 'Albert Einstein'
+message = famous_person + ' once said,"A person who never made a mistake never tried anything new."'
+print(message)
+print('\n')
+
+username = '\tEric\n'
+print(username.lstrip())
+print(username.rstrip())
+print(username.strip())
+
+
+
